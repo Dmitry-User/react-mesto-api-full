7dd316f367f84055007dbc942173b43d67bdc4e7
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-09 20:44:36 +0700

    feat: added npm CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const limiter = require('express-rate-limit');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
@@ -18,16 +19,22 @@ mongoose.connect(MONGO_URL);
 
 const app = express();
 
+app.use(requestLogger); // логгер запросов
 app.use(limiter({
   windowMs: 10 * 60 * 1000, // за 10 минут
   max: 100, // можно совершить максимум 100 запросов с одного IP
 }));
-
+app.use(cors({
+  origin: [
+    'https://goodplaces.nomoredomains.icu',
+    'http://goodplaces.nomoredomains.icu',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(helmet());
 app.use(express.json()); // встроенный метод, вместо body-parser
-
-app.use(requestLogger); // логгер запросов
 app.use(router);
 
 app.use(errorLogger); // логгер ошибок
