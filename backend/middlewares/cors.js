bd56798148af8871c2147b348d463d6e772b10b2
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-14 21:25:01 +0700

    fix: bug fixed cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,9 +10,10 @@ const cors = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
